f76f44bbb27cd6e57d63c1a13c8a68da5315f063
Haile4782 <102994683+Haile4782@users.noreply.github.com>
2025-11-23 11:47:39 +0300

    Update Vending Machine Program.py

--- 1. Vending Machine Program.py
+++ 1. Vending Machine Program.py
@@ -1,17 +1,3 @@
-
-# 1. Vending machine
-
-# Build a program that:
-# Displays a list of snacks and drinks with item numbers and prices.
-# Asks the user to choose items by number in a loop.
-# Keeps track of selected items and their prices.
-# Ends when the user types “done”.
-# Finally prints a receipt showing:
-# List of selected items with prices
-# Total cost
-# Skills practiced: loops, input(), conditionals, lists/dictionaries, sum(), print formatting
-
-
 def vending_machine():
     # Inventory: ID mapping to Dictionary of details
     inventory = {
